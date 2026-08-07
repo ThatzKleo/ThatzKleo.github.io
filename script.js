fccb1c1aa19d9e9b5d1c2fe373c2d4388e9f5eec
ThatzKleo <ThatzKleoCS@interia.pl>
2026-08-07 22:13:12 +0200

    adding other updates

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function collapseandexpand() {
-    const div = document.getElementById("Expand");
-    div.classList.toggle("open");
+function siteCandE() {
+    const site = document.getElementById("Expand");
+    site.classList.toggle("open");
 
     const btn = document.querySelector('button[onclick="collapseandexpand()"]');
-    btn.textContent = div.classList.contains("open") ? "Show less" : "Show more";
+    btn.textContent = site.classList.contains("open") ? "Show less" : "Show more";
+}
+function otherCandE() {
+    const other = document.getElementById("Expand1");
+    other.classList.toggle("open");
+
+    const btn = document.querySelector('button[onclick="collapseandexpand()"]');
+    btn.textContent = other.classList.contains("open") ? "Show less" : "Show more";
 }
